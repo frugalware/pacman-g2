c4d5fd301b3259c49e46274eef1d7d90daf7970d
Christian Hamar alias krix <krics@linuxforum.hu>
2005-12-12 19:02:51 +0000

    pacman: sha1 initial import   * Imported sha1.c and sha1.h

--- lib/libalpm/md5.h
+++ lib/libalpm/md5.h
@@ -23,7 +23,6 @@ These notices must be retained in any copies of any part of this
 documentation and/or software.
  */
 
-
 /* POINTER defines a generic pointer type */
 typedef unsigned char *POINTER;
 
