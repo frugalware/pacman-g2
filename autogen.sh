8d0931f0221ebba9e853255755dee83ec2d84a3a
VMiklos <vmiklos@frugalware.org>
2005-12-16 00:00:51 +0000

    autogen.sh: added a --dist option impelements something like 'make dist' just we're automake monkeys^H^H^Husers

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -exu
 
+if [ "$1" == "--dist" ]; then
+	ver=`grep AC_INIT configure.in|sed 's/.*, \([^[,]*\), .*/\1/'`
+	darcs dist -d pacman-$ver
+	[ -d ../releases ] && mv pacman-$ver.tar.gz ../releases
+fi
+
 #intltoolize -f -c
 libtoolize -f -c
 aclocal --force
