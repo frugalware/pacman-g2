a59544836ed1f396c93e60cb6afe71be08e594fd
VMiklos <vmiklos@frugalware.org>
2006-04-21 21:00:25 +0000

    dist/FrugalBuild.in: handle sha1sums()

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 
 if [ "$1" == "--dist" ]; then
 	if [ -d ../releases ]; then
@@ -7,8 +7,6 @@ if [ "$1" == "--dist" ]; then
 	ver=`grep AC_INIT configure.ac|sed 's/.*, \([0-9\.]*\), .*/\1/'`
 	if [ ! "$release" ]; then
 		ver="${ver}_`date +%Y%m%d`"
-		sed "s/@PACKAGE_VERSION@/$ver" dist/FrugalBuild.in >
-			dist/FrugalBuild
 	fi
 	darcs changes >_darcs/current/ChangeLog
 	darcs dist -d pacman-$ver
@@ -20,6 +18,9 @@ if [ "$1" == "--dist" ]; then
 	fi
 	mv pacman-$ver.tar.gz $dest
 	if [ ! "$release" ]; then
+		sed "s/@PACKAGE_VERSION@/$ver/;
+			s/@SHA1SUM/`sha1sum $dest/pacman-$ver.tar.gz|sed 's/  .*//'`" \
+			dist/FrugalBuild.in > dist/FrugalBuild
 		echo "Now type: 'cd dist; makepkg -ci'."
 	fi
 	exit 0
