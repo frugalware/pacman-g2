587a2e9d455ae623ac8d0639eae00e7d2f96a03f
VMiklos <vmiklos@frugalware.org>
2007-04-07 11:57:20 +0000

    added a python example

---
+++ doc/examples/example.py
@@ -0,0 +1,6 @@
+import pacman
+
+# this will return with success (0)
+print pacman.initialize("/")
+# this will return with failure (-1) as the lib is already initialized
+print pacman.initialize("/")
