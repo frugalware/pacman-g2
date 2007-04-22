1c74c8bdd18686ab4364f124806ff8bc58810adc
VMiklos <vmiklos@frugalware.org>
2007-04-22 21:45:20 +0000

    autogen.sh: handle the case when there is no /usr/share/automake symlink

--- autogen.sh
+++ autogen.sh
@@ -117,7 +117,7 @@ libtoolize -f -c
 aclocal --force
 autoheader -f
 autoconf -f
-cp -f $(dirname $(which automake))/../share/automake/mkinstalldirs ./
+cp -f $(dirname $(which automake))/../share/automake-$(automake --version|sed 's/.* //;q')/mkinstalldirs ./
 cp -f $(dirname $(which automake))/../share/gettext/config.rpath ./
 automake -a -c --gnu --foreign
 
