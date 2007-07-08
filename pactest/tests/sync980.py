ed3ae724bb7d65c5d2a7413c308a757bd90c0180
VMiklos <vmiklos@frugalware.org>
2007-07-08 13:41:14 +0000

    pactest/tests/sync98*: new tests for -R vs provides

---
+++ pactest/tests/sync980.py
@@ -0,0 +1,25 @@
+self.description = "Remove with provides"
+
+lp1 = pmpkg("hunspell")
+lp1.depends = ["hunspell-dict"]
+lp1.requiredby = ['hunspell-hu', 'hunspell-de']
+self.addpkg2db("local", lp1);
+
+lp2 = pmpkg("hunspell-de")
+lp2.provides = ["hunspell-dict"]
+lp2.depends = ["hunspell"]
+lp2.requiredby = ['hunspell']
+self.addpkg2db("local", lp2)
+
+lp3 = pmpkg("hunspell-hu")
+lp3.provides = ["hunspell-dict"]
+lp2.depends = ["hunspell"]
+lp2.requiredby = ['hunspell']
+self.addpkg2db("local", lp3)
+
+self.args = "-R hunspell-hu"
+
+self.addrule("PACMAN_RETCODE=0")
+self.addrule("PKG_EXIST=hunspell")
+self.addrule("PKG_EXIST=hunspell-de")
+self.addrule("!PKG_EXIST=hunspell-hu")
