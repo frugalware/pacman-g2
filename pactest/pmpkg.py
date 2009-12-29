690d0bc6d4e1c0a01c849554e4cc3a826c96f5d6
Michel Hermier <hermier@frugalware.org>
2009-12-29 13:28:16 +0100

    pactest/*.py: replace '\t' with '    ' to ease merge.

--- pactest/pmpkg.py
+++ pactest/pmpkg.py
@@ -27,165 +27,165 @@ from util import *
 
 
 class pmpkg:
-	"""Package object.
-	
-	Object holding data from a pacman-g2 package.
-	"""
+    """Package object.
+    
+    Object holding data from a pacman-g2 package.
+    """
 
-	def __init__(self, name, version = "1.0-1", arch = os.uname()[-1]):
-		# desc
-		self.name = name
-		self.version = version
-		self.desc = ""
-		self.groups = []
-		self.url = ""
-		self.license = []
-		self.arch = arch
-		self.builddate = ""
-		self.installdate = ""
-		self.packager = ""
-		self.size = 0
-		self.csize = 0
-		self.reason = 0
-		self.sha1sum = ""      # sync only
-		self.replaces = []    # sync only
-		self.force = 0        # sync only
-		self.stick = 0        # sync only
-		# depends
-		self.depends = []
-		self.requiredby = []  # local only
-		self.conflicts = []
-		self.provides = []
-		# files
-		self.files = []
-		self.backup = []
-		self.removes = []
-		# install
-		self.install = {
-			"pre_install": "",
-			"post_install": "",
-			"pre_remove": "",
-			"post_remove": "",
-			"pre_upgrade": "",
-			"post_upgrade": ""
-		}
-		self.checksum = {
-			"desc": "",
-			"depends": "",
-			"files": "",
-			"install": ""
-		}
-		self.mtime = {
-			"desc": (0, 0, 0),
-			"depends": (0, 0, 0),
-			"files": (0, 0, 0),
-			"install": (0, 0, 0)
-		}
+    def __init__(self, name, version = "1.0-1", arch = os.uname()[-1]):
+        # desc
+        self.name = name
+        self.version = version
+        self.desc = ""
+        self.groups = []
+        self.url = ""
+        self.license = []
+        self.arch = arch
+        self.builddate = ""
+        self.installdate = ""
+        self.packager = ""
+        self.size = 0
+        self.csize = 0
+        self.reason = 0
+        self.sha1sum = ""      # sync only
+        self.replaces = []    # sync only
+        self.force = 0        # sync only
+        self.stick = 0        # sync only
+        # depends
+        self.depends = []
+        self.requiredby = []  # local only
+        self.conflicts = []
+        self.provides = []
+        # files
+        self.files = []
+        self.backup = []
+        self.removes = []
+        # install
+        self.install = {
+            "pre_install": "",
+            "post_install": "",
+            "pre_remove": "",
+            "post_remove": "",
+            "pre_upgrade": "",
+            "post_upgrade": ""
+        }
+        self.checksum = {
+            "desc": "",
+            "depends": "",
+            "files": "",
+            "install": ""
+        }
+        self.mtime = {
+            "desc": (0, 0, 0),
+            "depends": (0, 0, 0),
+            "files": (0, 0, 0),
+            "install": (0, 0, 0)
+        }
 
-	def __str__(self):
-		s = ["%s" % self.fullname()]
-		s.append("description: %s" % self.desc)
-		s.append("url: %s" % self.url)
-		s.append("depends: %s" % " ".join(self.depends))
-		s.append("files: %s" % " ".join(self.files))
-		s.append("reason: %d" % self.reason)
-		return "\n".join(s)
+    def __str__(self):
+        s = ["%s" % self.fullname()]
+        s.append("description: %s" % self.desc)
+        s.append("url: %s" % self.url)
+        s.append("depends: %s" % " ".join(self.depends))
+        s.append("files: %s" % " ".join(self.files))
+        s.append("reason: %d" % self.reason)
+        return "\n".join(s)
 
-	def dbname(self):
-		"""Db name of a package.
-		
-		Returns a string formatted as follows: "pkgname-pkgver".
-		"""
-		return self.name + "-" + self.version
+    def dbname(self):
+        """Db name of a package.
+        
+        Returns a string formatted as follows: "pkgname-pkgver".
+        """
+        return self.name + "-" + self.version
 
-	def fullname(self):
-		"""Long name of a package.
-		
-		Returns a string formatted as follows: "pkgname-pkgver-arch".
-		"""
-		return self.name + "-" + self.version + "-" + self.arch
+    def fullname(self):
+        """Long name of a package.
+        
+        Returns a string formatted as follows: "pkgname-pkgver-arch".
+        """
+        return self.name + "-" + self.version + "-" + self.arch
 
-	def filename(self):
-		"""File name of a package, including its extension.
-		
-		Returns a string formatted as follows: "pkgname-pkgver.PKG_EXT_PKG".
-		"""
-		return "%s%s" % (self.fullname(), PM_EXT_PKG)
+    def filename(self):
+        """File name of a package, including its extension.
+        
+        Returns a string formatted as follows: "pkgname-pkgver.PKG_EXT_PKG".
+        """
+        return "%s%s" % (self.fullname(), PM_EXT_PKG)
 
-	def install_files(self, root):
-		"""Install files in the filesystem located under "root".
-		
-		Files are created with content generated automatically.
-		"""
-		[mkfile(os.path.join(root, f), f) for f in self.files]
+    def install_files(self, root):
+        """Install files in the filesystem located under "root".
+        
+        Files are created with content generated automatically.
+        """
+        [mkfile(os.path.join(root, f), f) for f in self.files]
 
-	def makepkg(self, path):
-		"""Creates a pacman package archive.
-		
-		A package archive is generated in the location 'path', based on the data
-		from the object.
-		"""
-		archive = os.path.join(path, self.filename())
+    def makepkg(self, path):
+        """Creates a pacman package archive.
+        
+        A package archive is generated in the location 'path', based on the data
+        from the object.
+        """
+        archive = os.path.join(path, self.filename())
 
-		curdir = os.getcwd()
-		tmpdir = tempfile.mkdtemp()
-		os.chdir(tmpdir)
+        curdir = os.getcwd()
+        tmpdir = tempfile.mkdtemp()
+        os.chdir(tmpdir)
 
-		# Generate package file system
-		for f in self.files:
-			mkfile(f, f)
-			self.size += os.stat(getfilename(f))[stat.ST_SIZE]
+        # Generate package file system
+        for f in self.files:
+            mkfile(f, f)
+            self.size += os.stat(getfilename(f))[stat.ST_SIZE]
 
-		# .PKGINFO
-		data = ["pkgname = %s" % self.name]
-		data.append("pkgver = %s" % self.version)
-		data.append("pkgdesc = %s" % self.desc)
-		data.append("url = %s" % self.url)
-		data.append("builddate = %s" % self.builddate)
-		data.append("packager = %s" % self.packager)
-		data.append("size = %s" % self.size)
-		if self.arch:
-			data.append("arch = %s" % self.arch)
-		for i in self.license:
-			data.append("license = %s" % i)
-		for i in self.replaces:
-			data.append("replaces = %s" % i)
-		for i in self.groups:
-			data.append("group = %s" % i)
-		for i in self.depends:
-			data.append("depend = %s" % i)
-		for i in self.conflicts:
-			data.append("conflict = %s" % i)
-		for i in self.provides:
-			data.append("provides = %s" % i)
-		for i in self.backup:
-			data.append("backup = %s" % i)
-		for i in self.removes:
-			data.append("remove = %s" % i)
-		mkfile(".PKGINFO", "\n".join(data))
-		targets = ".PKGINFO"
+        # .PKGINFO
+        data = ["pkgname = %s" % self.name]
+        data.append("pkgver = %s" % self.version)
+        data.append("pkgdesc = %s" % self.desc)
+        data.append("url = %s" % self.url)
+        data.append("builddate = %s" % self.builddate)
+        data.append("packager = %s" % self.packager)
+        data.append("size = %s" % self.size)
+        if self.arch:
+            data.append("arch = %s" % self.arch)
+        for i in self.license:
+            data.append("license = %s" % i)
+        for i in self.replaces:
+            data.append("replaces = %s" % i)
+        for i in self.groups:
+            data.append("group = %s" % i)
+        for i in self.depends:
+            data.append("depend = %s" % i)
+        for i in self.conflicts:
+            data.append("conflict = %s" % i)
+        for i in self.provides:
+            data.append("provides = %s" % i)
+        for i in self.backup:
+            data.append("backup = %s" % i)
+        for i in self.removes:
+            data.append("remove = %s" % i)
+        mkfile(".PKGINFO", "\n".join(data))
+        targets = ".PKGINFO"
 
-		# .INSTALL
-		empty = 1
-		for value in self.install.values():
-			if value:
-				empty = 0
-		if not empty:
-			mkinstallfile(".INSTALL", self.install)
-			targets += " .INSTALL"
+        # .INSTALL
+        empty = 1
+        for value in self.install.values():
+            if value:
+                empty = 0
+        if not empty:
+            mkinstallfile(".INSTALL", self.install)
+            targets += " .INSTALL"
 
-		# .FILELIST
-		if self.files:
-			os.system("tar cvf /dev/null * | sort >.FILELIST")
-			targets += " .FILELIST *"
+        # .FILELIST
+        if self.files:
+            os.system("tar cvf /dev/null * | sort >.FILELIST")
+            targets += " .FILELIST *"
 
-		# Generate package archive
-		os.system("tar zcf %s %s" % (archive, targets))
+        # Generate package archive
+        os.system("tar zcf %s %s" % (archive, targets))
 
-		os.chdir(curdir)
-		shutil.rmtree(tmpdir)
+        os.chdir(curdir)
+        shutil.rmtree(tmpdir)
 
 
 if __name__ == "__main__":
-	pkg = pmpkg("dummy")
-	print pkg
+    pkg = pmpkg("dummy")
+    print pkg
--- pactest/util.py
+++ pactest/util.py
@@ -50,16 +50,16 @@ debug = 0
 
 
 def err(msg):
-	print "error: " + msg
-	sys.exit(1)
+    print "error: " + msg
+    sys.exit(1)
 
 def vprint(msg):
-	if verbose:
-		print msg
+    if verbose:
+        print msg
 
 def dbg(msg):
-	if debug:
-		print msg
+    if debug:
+        print msg
 
 
 #
@@ -67,135 +67,135 @@ def dbg(msg):
 #
 
 def getfilename(name):
-	"""
-	"""
-	filename = ""
-	link = ""
-	if not name.find(" -> ") == -1:
-		filename, link = name.split(" -> ")
-	elif name[-1] == "*":
-		filename = name.rstrip("*")
-	else:
-		filename = name
-	return filename
+    """
+    """
+    filename = ""
+    link = ""
+    if not name.find(" -> ") == -1:
+        filename, link = name.split(" -> ")
+    elif name[-1] == "*":
+        filename = name.rstrip("*")
+    else:
+        filename = name
+    return filename
 
 def mkfile(name, data = ""):
-	"""
-	"""
+    """
+    """
 
-	isaltered = 0
-	isdir = 0
-	islink = 0
-	link = ""
-	filename = ""
+    isaltered = 0
+    isdir = 0
+    islink = 0
+    link = ""
+    filename = ""
 
-	if not name.find(" -> ") == -1:
-		islink = 1
-		filename, link = name.split(" -> ")
-	elif name[-1] == "*":
-		isaltered = 1
-		filename = name.rstrip("*")
-	else:
-		filename = name
-	if name[-1] == "/":
-		isdir = 1
+    if not name.find(" -> ") == -1:
+        islink = 1
+        filename, link = name.split(" -> ")
+    elif name[-1] == "*":
+        isaltered = 1
+        filename = name.rstrip("*")
+    else:
+        filename = name
+    if name[-1] == "/":
+        isdir = 1
 
-	if isdir:
-		path = filename
-	else:
-		path = os.path.dirname(filename)
-	try:
-		if path and not os.path.isdir(path):
-			os.makedirs(path, 0755)
-	except:
-		error("mkfile: could not create directory hierarchy '%s'" % path)
+    if isdir:
+        path = filename
+    else:
+        path = os.path.dirname(filename)
+    try:
+        if path and not os.path.isdir(path):
+            os.makedirs(path, 0755)
+    except:
+        error("mkfile: could not create directory hierarchy '%s'" % path)
 
-	if isdir:
-		return
-	if islink:
-		curdir = os.getcwd()
-		os.chdir(path)
-		os.symlink(link, os.path.basename(filename))
-		os.chdir(curdir)
-	else:
-		fd = file(filename, "w")
-		if data:
-			fd.write(data)
-			if data[-1] != "\n":
-				fd.write("\n")
-		fd.close()
+    if isdir:
+        return
+    if islink:
+        curdir = os.getcwd()
+        os.chdir(path)
+        os.symlink(link, os.path.basename(filename))
+        os.chdir(curdir)
+    else:
+        fd = file(filename, "w")
+        if data:
+            fd.write(data)
+            if data[-1] != "\n":
+                fd.write("\n")
+        fd.close()
 
 def mkdescfile(filename, pkg):
-	"""
-	"""
+    """
+    """
 
-	data = []
+    data = []
 
-	# desc
-	#data.append("pkgname = %s" % pkg.name)
-	#data.append("pkgver = %s" % pkg.version)
-	if pkg.desc:
-		data.append("pkgdesc = %s" % pkg.desc)
-	if pkg.url:
-		data.append("url = %s" % pkg.url)
-	if pkg.builddate:
-		data.append("builddate = %s" % pkg.builddate)
-	if pkg.packager:
-		data.append("packager = %s" % pkg.packager)
-	if pkg.size:
-		data.append("size = %s" % pkg.size)
-	if pkg.arch:
-		data.append("arch = %s" % pkg.arch)
-	for i in pkg.groups:
-		data.append("group = %s" % i)
-	for i in pkg.license:
-		data.append("license = %s" % i)
-	if pkg.sha1sum:
-		data.append("sha1sum = %s" % pkg.sha1sum)
+    # desc
+    #data.append("pkgname = %s" % pkg.name)
+    #data.append("pkgver = %s" % pkg.version)
+    if pkg.desc:
+        data.append("pkgdesc = %s" % pkg.desc)
+    if pkg.url:
+        data.append("url = %s" % pkg.url)
+    if pkg.builddate:
+        data.append("builddate = %s" % pkg.builddate)
+    if pkg.packager:
+        data.append("packager = %s" % pkg.packager)
+    if pkg.size:
+        data.append("size = %s" % pkg.size)
+    if pkg.arch:
+        data.append("arch = %s" % pkg.arch)
+    for i in pkg.groups:
+        data.append("group = %s" % i)
+    for i in pkg.license:
+        data.append("license = %s" % i)
+    if pkg.sha1sum:
+        data.append("sha1sum = %s" % pkg.sha1sum)
 
-	# depends
-	for i in pkg.replaces:
-		data.append("replaces = %s" % i)
-	for i in pkg.depends:
-		data.append("depend = %s" % i)
-	for i in pkg.conflicts:
-		data.append("conflict = %s" % i)
-	for i in pkg.provides:
-		data.append("provides = %s" % i)
-	for i in pkg.backup:
-		data.append("backup = %s" % i)
-	for i in pkg.removes:
-		data.append("remove = %s" % i)
-	if pkg.force:
-		data.append("force = 1")
+    # depends
+    for i in pkg.replaces:
+        data.append("replaces = %s" % i)
+    for i in pkg.depends:
+        data.append("depend = %s" % i)
+    for i in pkg.conflicts:
+        data.append("conflict = %s" % i)
+    for i in pkg.provides:
+        data.append("provides = %s" % i)
+    for i in pkg.backup:
+        data.append("backup = %s" % i)
+    for i in pkg.removes:
+        data.append("remove = %s" % i)
+    if pkg.force:
+        data.append("force = 1")
 
-	mkfile(filename, "\n".join(data))
+    mkfile(filename, "\n".join(data))
 
 def mkinstallfile(filename, install):
-	"""
-	"""
-	data = []
-	for key, value in install.iteritems():
-		if value:
-			data.append("%s() {\n%s\n}" % (key, value))
-			
-	mkfile(filename, "\n".join(data))
+    """
+    """
+    data = []
+    for key, value in install.iteritems():
+        if value:
+            data.append("%s() {\n%s\n}" % (key, value))
+            
+    mkfile(filename, "\n".join(data))
 
 def mkcfgfile(filename, root, option, db):
-	"""
-	"""
-	# Options
-	data = ["[options]"]
-	for key, value in option.iteritems():
-		data.extend(["%s = %s" % (key, j) for j in value])
+    """
+    """
+    # Options
+    data = ["[options]"]
+    for key, value in option.iteritems():
+        data.extend(["%s = %s" % (key, j) for j in value])
 
-	# Repositories
-	data.extend(["[%s]\n" \
-	             "server = file://%s\n" \
-	             % (value.treename, os.path.join(root, SYNCREPO, value.treename)) \
-	             for key, value in db.iteritems() if not key == "local"])
+    # Repositories
+    data.extend(["[%s]\n" \
+                 "server = file://%s\n" \
+                 % (value.treename, os.path.join(root, SYNCREPO, value.treename)) \
+                 for key, value in db.iteritems() if not key == "local"])
 
-	mkfile(os.path.join(root, filename), "\n".join(data))
+    mkfile(os.path.join(root, filename), "\n".join(data))
 
 
 #
@@ -203,29 +203,29 @@ def mkcfgfile(filename, root, option, db):
 #
 
 def getmd5sum(filename):
-	"""
-	"""
-	checksum = hashlib.new("md5")
-	try:
-		fd = open(filename, "rb")
-		while 1:
-			block = fd.read(1048576)
-			if not block:
-				break
-			checksum.update(block)
-		fd.close()
-	except IOError:
-		pass
-	digest = checksum.digest()
-	return "%02x"*len(digest) % tuple(map(ord, digest))
+    """
+    """
+    checksum = hashlib.new("md5")
+    try:
+        fd = open(filename, "rb")
+        while 1:
+            block = fd.read(1048576)
+            if not block:
+                break
+            checksum.update(block)
+        fd.close()
+    except IOError:
+        pass
+    digest = checksum.digest()
+    return "%02x"*len(digest) % tuple(map(ord, digest))
 
 def mkmd5sum(data):
-	"""
-	"""
-	checksum = hashlib.new("md5")
-	checksum.update("%s\n" % data)
-	digest = checksum.digest()
-	return "%02x"*len(digest) % tuple(map(ord, digest))
+    """
+    """
+    checksum = hashlib.new("md5")
+    checksum.update("%s\n" % data)
+    digest = checksum.digest()
+    return "%02x"*len(digest) % tuple(map(ord, digest))
 
 
 #
@@ -233,29 +233,29 @@ def mkmd5sum(data):
 #
 
 def getsha1sum(filename):
-	"""
-	"""
-	checksum = hashlib.new("sha1")
-	try:
-		fd = open(filename, "rb")
-		while 1:
-			block = fd.read(1048576)
-			if not block:
-				break
-			checksum.update(block)
-		fd.close()
-	except IOError:
-		pass
-	digest = checksum.digest()
-	return "%02x"*len(digest) % tuple(map(ord, digest))
+    """
+    """
+    checksum = hashlib.new("sha1")
+    try:
+        fd = open(filename, "rb")
+        while 1:
+            block = fd.read(1048576)
+            if not block:
+                break
+            checksum.update(block)
+        fd.close()
+    except IOError:
+        pass
+    digest = checksum.digest()
+    return "%02x"*len(digest) % tuple(map(ord, digest))
 
 def mksha1sum(data):
-	"""
-	"""
-	checksum = hashlib.new("sha1")
-	checksum.update("%s\n" % data)
-	digest = checksum.digest()
-	return "%02x"*len(digest) % tuple(map(ord, digest))
+    """
+    """
+    checksum = hashlib.new("sha1")
+    checksum.update("%s\n" % data)
+    digest = checksum.digest()
+    return "%02x"*len(digest) % tuple(map(ord, digest))
 
 
 #
@@ -263,10 +263,10 @@ def mksha1sum(data):
 #
 
 def getmtime(filename):
-	"""
-	"""
-	st = os.lstat(filename)
-	return st[stat.ST_ATIME], st[stat.ST_MTIME], st[stat.ST_CTIME]
+    """
+    """
+    st = os.lstat(filename)
+    return st[stat.ST_ATIME], st[stat.ST_MTIME], st[stat.ST_CTIME]
 
 
 #
@@ -274,17 +274,17 @@ def getmtime(filename):
 #
 
 def grep(filename, pattern):
-	found = 0
-	fd = file(filename, "r")
-	while 1 and not found:
-		line = fd.readline()
-		if not line:
-			break
-		if line.find(pattern) != -1:
-			found = 1
-	fd.close()
-	return found
+    found = 0
+    fd = file(filename, "r")
+    while 1 and not found:
+        line = fd.readline()
+        if not line:
+            break
+        if line.find(pattern) != -1:
+            found = 1
+    fd.close()
+    return found
 
 
 if __name__ == "__main__":
-	pass
+    pass
