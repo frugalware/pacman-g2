37267f5048a58a5e07cc70d01e5e7dfab0fd378a
Michel Hermier <hermier@frugalware.org>
2013-03-07 10:58:01 +0100

    libpacman: Fix some warnings.

--- lib/libpacman/packages_transaction.c
+++ lib/libpacman/packages_transaction.c
@@ -39,9 +39,10 @@ _pacman_packages_transaction_set_state(pmtrans_t *trans, int new_state)
 
 int _pacman_packages_transaction_init(pmtrans_t *trans)
 {
-	ASSERT(trans != NULL, RET_ERR(PM_ERR_TRANS_NULL, NULL));
+	ASSERT(trans != NULL, RET_ERR(PM_ERR_TRANS_NULL, -1));
 
 	trans->set_state = _pacman_packages_transaction_set_state;
+	return 0;
 }
 
 /* vim: set ts=2 sw=2 noet: */
