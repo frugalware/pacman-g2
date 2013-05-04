cc4892304f1292eb8a9adfc0faba66d7ae103a45
Michel Hermier <hermier@frugalware.org>
2013-05-04 12:17:41 +0200

    libflib: Add get set accessors to FList items.

--- lib/libflib/flist.c
+++ lib/libflib/flist.c
@@ -80,6 +80,33 @@ FList *f_list_free (FList *item, FVisitorFunc fn, void *user_data) {
 	return next;
 }
 
+void *f_list_get (FList *item) {
+	return item != NULL ? item->data : NULL;
+}
+
+void f_list_set (FList *item, void *data) {
+	if (item) {
+		item->data = data;
+	}
+}
+
+size_t f_list_count(FList *list)
+{
+	size_t count;
+
+	for (count = 0; list != NULL; list = list->next, count++)
+		/* Do nothing */;
+	return count;
+}
+
+FList *f_list_first (FList *list) {
+	if (list != NULL) {
+		for (; list->prev != NULL; list = list->prev)
+			/* Do nothing */;
+	}
+	return list;
+}
+
 void f_list_insert_after (FList *item, FList *list) {
 	FList *last = f_list_last (list);
 
@@ -108,23 +135,6 @@ void f_list_insert_before (FList *item, FList *list) {
 	if (item->prev != NULL) {
 		item->prev->next = list;
 	}
-}
-
-size_t f_list_count(FList *list)
-{
-	size_t count;
-
-	for (count = 0; list != NULL; list = list->next, count++)
-		/* Do nothing */;
-	return count;
-}
-
-FList *f_list_first (FList *list) {
-	if (list != NULL) {
-		for (; list->prev != NULL; list = list->prev)
-			/* Do nothing */;
-	}
-	return list;
 }
 
 FList *f_list_last (FList *list) {
