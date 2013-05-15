3743871b0bc8742d27f11d7d729594c2e029c2f7
Michel Hermier <hermier@frugalware.org>
2013-05-15 08:20:11 +0200

    libflib: Move SHA1 hash code from libpacman to flib.

--- lib/libpacman/sha1.h
+++ lib/libpacman/sha1.h
@@ -16,45 +16,5 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-#include <stdio.h>
-#include <limits.h>
-
-#define rol(x,n) ( ((x) << (n)) | ((x) >> (32 -(n))) )
-/* The code below is from md5.h (from coreutils), little modifications */
-#define UINT_MAX_32_BITS 4294967295U
-
-#if UINT_MAX == UINT_MAX_32_BITS
-    typedef unsigned int sha_uint32;
-#else
-#if USHRT_MAX == UINT_MAX_32_BITS
-    typedef unsigned short sha_uint32;
-#else
-#if ULONG_MAX == UINT_MAX_32_BITS
-    typedef unsigned long sha_uint32;
-#else
-    /* The following line is intended to evoke an error. Using #error is not portable enough.  */
-#error "Cannot determine unsigned 32-bit data type"
-#endif
-#endif
-#endif
-/* We have to make a guess about the integer type equivalent in size
-   to pointers which should always be correct.  */
-typedef unsigned long int sha_uintptr;
-
-/* Structure to save state of computation between the single steps.  */
-struct sha_ctx
-{
-  sha_uint32 A;
-  sha_uint32 B;
-  sha_uint32 C;
-  sha_uint32 D;
-  sha_uint32 E;
-
-  sha_uint32 total[2];
-  sha_uint32 buflen;
-  char buffer[128];
-};
-
-
 /* Needed for pacman-g2 */
 char *_pacman_SHAFile (char *);
