1fcec1acef44531a6eaf0a265e1dfc7fdc347abd
James Buren <ryuo@frugalware.org>
2013-06-30 06:58:27 -0500

    pacman_db_getinfo: replace pmserver_t type usage with libfetch API it seems to be an exact replacement.

--- lib/libpacman/pacman.c
+++ lib/libpacman/pacman.c
@@ -233,8 +233,7 @@ int pacman_db_unregister(pmdb_t *db)
 void *pacman_db_getinfo(PM_DB *db, unsigned char parm)
 {
 	void *data = NULL;
-	char path[PATH_MAX];
-	pmserver_t *server;
+	struct url *server;
 
 	/* Sanity checks */
 	ASSERT(handle != NULL, return(NULL));
@@ -243,14 +242,8 @@ void *pacman_db_getinfo(PM_DB *db, unsigned char parm)
 	switch(parm) {
 		case PM_DB_TREENAME:   data = db->treename; break;
 		case PM_DB_FIRSTSERVER:
-			server = (pmserver_t*)db->servers->data;
-			if(!strcmp(server->protocol, "file")) {
-				snprintf(path, PATH_MAX, "%s://%s", server->protocol, server->path);
-			} else {
-				snprintf(path, PATH_MAX, "%s://%s%s", server->protocol,
-						server->server, server->path);
-			}
-			data = strdup(path);
+			server = (struct url *)db->servers->data;
+			data = fetchStringifyURL(server);
 		break;
 		default:
 			free(data);
