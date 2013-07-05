c24c2f24b56692bb0d39fe583013227cf922fcdd
James Buren <ryuo@frugalware.org>
2013-07-05 12:05:26 -0500

    server: convert _pacman_fetch_pkgurl to use new internal API

--- lib/libpacman/server.c
+++ lib/libpacman/server.c
@@ -499,31 +499,15 @@ int _pacman_downloadfiles_forreal(pmlist_t *servers, const char *localpath,
 
 char *_pacman_fetch_pkgurl(char *target)
 {
-	char spath[PATH_MAX], lpath[PATH_MAX], lcache[PATH_MAX];
-	char url[PATH_MAX];
-	char *host, *path, *fn;
+	char lpath[PATH_MAX], lcache[PATH_MAX];
+	char *fn;
 	struct stat buf;
 
-	strncpy(url, target, PATH_MAX);
-	host = strstr(url, "://");
-	*host = '\0';
-	host += 3;
-	path = strchr(host, '/');
-	*path = '\0';
-	path++;
-	fn = strrchr(path, '/');
-	if(fn) {
-		*fn = '\0';
-		if(path[0] == '/') {
-			snprintf(spath, PATH_MAX, "%s/", path);
-		} else {
-			snprintf(spath, PATH_MAX, "/%s/", path);
-		}
-		fn++;
-	} else {
-		fn = path;
-		strcpy(spath, "/");
-	}
+	if((fn = strrchr(target,'/')) == NULL)
+		return NULL;
+
+	++fn;
+
 	snprintf(lcache, PATH_MAX, "%s%s", handle->root, handle->cachedir);
 	snprintf(lpath, PATH_MAX, "%s%s/%s", handle->root, handle->cachedir, fn);
 
@@ -532,17 +516,15 @@ char *_pacman_fetch_pkgurl(char *target)
 	if(stat(lpath, &buf) == 0) {
 		_pacman_log(PM_LOG_DEBUG, _("%s is already in the cache\n"), fn);
 	} else {
-		pmserver_t *server;
-		pmlist_t *servers = NULL;
+		struct url *server;
+		pmlist_t *servers;
 		pmlist_t *files;
 
-		if((server = _pacman_malloc(sizeof(pmserver_t))) == NULL) {
+		if((server = _pacman_server_new(target)) == NULL) {
 			return(NULL);
 		}
-		server->protocol = url;
-		server->server = host;
-		server->path = spath;
-		servers = _pacman_list_add(servers, server);
+		
+		servers = _pacman_list_add(NULL, server);
 
 		files = _pacman_list_add(NULL, fn);
 		if(_pacman_downloadfiles(servers, lcache, files, 0)) {
@@ -551,7 +533,7 @@ char *_pacman_fetch_pkgurl(char *target)
 		}
 		FREELISTPTR(files);
 
-		FREELIST(servers);
+		FREELISTSERVERS(servers);
 	}
 
 	/* return the target with the raw filename, no URL */
