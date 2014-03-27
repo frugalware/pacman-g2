ed8e320fe686cc894d2b81363e8175e724230763
Michel Hermier <hermier@frugalware.org>
2014-03-27 13:06:11 +0100

    libpacman: Add flog.{c,h} a generic logger functions.

---
+++ lib/libpacman/util/flog.c
@@ -0,0 +1,63 @@
+/*
+ *  flog.c
+ *
+ *  Copyright (c) 2014 by Michel Hermier <hermier@frugalware.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ */
+
+#include "config.h"
+
+#include <stdarg.h> /* for va_list */
+#include <stddef.h> /* for NULL */
+
+/* pacman-g2 */
+#include "util/flog.h"
+
+FLogger *_f_sys_logger = NULL;
+
+void f_log(unsigned char flag, const char *format, ...)
+{
+	va_list ap;
+
+	if (_f_sys_logger == NULL) {
+		return;
+	}
+
+	va_start(ap, format);
+	f_vlog(flag, format, ap);
+	va_end(ap);
+}
+
+void f_logs(unsigned char flag, const char *message)
+{
+	if(_f_sys_logger == NULL) {
+		return;
+	}
+
+	f_logger_logs(_f_sys_logger, flag, message);
+}
+
+void f_vlog(unsigned char flag, const char *format, va_list ap)
+{
+	if(_f_sys_logger == NULL) {
+		return;
+	}
+
+	f_logger_vlog(_f_sys_logger, flag, format, ap);
+}
+
+/* vim: set ts=2 sw=2 noet: */
---
+++ lib/libpacman/util/flog.h
@@ -0,0 +1,32 @@
+/*
+ *  flog.h
+ *
+ *  Copyright (c) 2014 by Michel Hermier <hermier@frugalware.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ *  USA.
+ */
+#ifndef F_LOG_H
+#define F_LOG_H
+
+#include "flogger.h"
+
+void f_log(unsigned char flag, const char *format, ...);
+void f_logs(unsigned char flag, const char *message);
+void f_vlog(unsigned char flag, const char *format, va_list ap);
+
+#endif /* F_LOG_H */
+
+/* vim: set ts=2 sw=2 noet: */
