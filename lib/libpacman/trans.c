771922a4a2690f2f29f9097a1d0dcd7fe038bf12
Michel Hermier <hermier@frugalware.org>
2014-04-01 14:12:57 +0200

    libpacman: Merge remove.c in trans.c.

--- lib/libpacman/trans.c
+++ lib/libpacman/trans.c
@@ -35,7 +35,6 @@
 #include "package.h"
 #include "util.h"
 #include "handle.h"
-#include "remove.h"
 #include "sync.h"
 #include "cache.h"
 #include "pacman.h"
@@ -54,6 +53,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+static
+int _pacman_remove_commit(pmtrans_t *trans, pmlist_t **data);
 
 static int check_oldcache(void)
 {
@@ -99,10 +101,8 @@ int _pacman_trans_init(pmtrans_t *trans, pmtranstype_t type, unsigned int flags,
 	switch(type) {
 		case PM_TRANS_TYPE_ADD:
 		case PM_TRANS_TYPE_UPGRADE:
-			trans->ops = NULL;
-		break;
 		case PM_TRANS_TYPE_REMOVE:
-			trans->ops = &_pacman_remove_pmtrans_opts;
+			trans->ops = NULL;
 		break;
 		case PM_TRANS_TYPE_SYNC:
 			trans->ops = &_pacman_sync_pmtrans_opts;
@@ -545,6 +545,65 @@ int _pacman_add_prepare(pmtrans_t *trans, pmlist_t **data)
 	return(0);
 }
 
+int _pacman_remove_prepare(pmtrans_t *trans, pmlist_t **data)
+{
+	pmlist_t *lp;
+	pmdb_t *db_local = trans->handle->db_local;
+
+	ASSERT(db_local != NULL, RET_ERR(PM_ERR_DB_NULL, -1));
+	ASSERT(trans != NULL, RET_ERR(PM_ERR_TRANS_NULL, -1));
+
+	if(!(trans->flags & (PM_TRANS_FLAG_NODEPS)) && (trans->type != PM_TRANS_TYPE_UPGRADE)) {
+		EVENT(trans, PM_TRANS_EVT_CHECKDEPS_START, NULL, NULL);
+
+		_pacman_log(PM_LOG_FLOW1, _("looking for unsatisfied dependencies"));
+		lp = _pacman_checkdeps(trans, trans->type, trans->packages);
+		if(lp != NULL) {
+			if(trans->flags & PM_TRANS_FLAG_CASCADE) {
+				while(lp) {
+					pmlist_t *i;
+					for(i = lp; i; i = i->next) {
+						pmdepmissing_t *miss = (pmdepmissing_t *)i->data;
+						pmpkg_t *pkg_local = _pacman_db_scan(db_local, miss->depend.name, INFRQ_ALL);
+						if(pkg_local) {
+							_pacman_log(PM_LOG_FLOW2, _("pulling %s in the targets list"), pkg_local->name);
+							trans->packages = _pacman_list_add(trans->packages, pkg_local);
+						} else {
+							_pacman_log(PM_LOG_ERROR, _("could not find %s in database -- skipping"),
+								miss->depend.name);
+						}
+					}
+					FREELIST(lp);
+					lp = _pacman_checkdeps(trans, trans->type, trans->packages);
+				}
+			} else {
+				if(data) {
+					*data = lp;
+				} else {
+					FREELIST(lp);
+				}
+				RET_ERR(PM_ERR_UNSATISFIED_DEPS, -1);
+			}
+		}
+
+		if(trans->flags & PM_TRANS_FLAG_RECURSE) {
+			_pacman_log(PM_LOG_FLOW1, _("finding removable dependencies"));
+			trans->packages = _pacman_removedeps(db_local, trans->packages);
+		}
+
+		/* re-order w.r.t. dependencies */
+		_pacman_log(PM_LOG_FLOW1, _("sorting by dependencies"));
+		lp = _pacman_sortbydeps(trans->packages, PM_TRANS_TYPE_REMOVE);
+		/* free the old alltargs */
+		FREELISTPTR(trans->packages);
+		trans->packages = lp;
+
+		EVENT(trans, PM_TRANS_EVT_CHECKDEPS_DONE, NULL, NULL);
+	}
+
+	return(0);
+}
+
 int _pacman_trans_prepare(pmtrans_t *trans, pmlist_t **data)
 {
 	/* Sanity checks */
@@ -572,7 +631,12 @@ int _pacman_trans_prepare(pmtrans_t *trans, pmlist_t **data)
 			return -1;
 		}
 	}
+	if(trans->type == PM_TRANS_TYPE_REMOVE) {
+		if(_pacman_remove_prepare(trans, data) == -1) {
+			return -1;
+		}
 	}
+}
 
 	_pacman_trans_set_state(trans, STATE_PREPARED);
 
@@ -1080,6 +1144,209 @@ int _pacman_add_commit(pmtrans_t *trans, pmlist_t **data)
 	return(ret);
 }
 
+/* Helper function for comparing strings
+ */
+static int str_cmp(const void *s1, const void *s2)
+{
+	return(strcmp(s1, s2));
+}
+
+int _pacman_remove_commit(pmtrans_t *trans, pmlist_t **data)
+{
+	pmpkg_t *pkg_local;
+	struct stat buf;
+	pmlist_t *targ, *lp;
+	char line[PATH_MAX+1];
+	int howmany, remain;
+	pmdb_t *db_local = trans->handle->db_local;
+
+	ASSERT(db_local != NULL, RET_ERR(PM_ERR_DB_NULL, -1));
+	ASSERT(trans != NULL, RET_ERR(PM_ERR_TRANS_NULL, -1));
+
+	howmany = _pacman_list_count(trans->packages);
+
+	for(targ = trans->packages; targ; targ = targ->next) {
+		int position = 0;
+		char pm_install[PATH_MAX];
+		pkg_local = (pmpkg_t*)targ->data;
+
+		if(handle->trans->state == STATE_INTERRUPTED) {
+			break;
+		}
+
+		remain = _pacman_list_count(targ);
+
+		if(trans->type != PM_TRANS_TYPE_UPGRADE) {
+			EVENT(trans, PM_TRANS_EVT_REMOVE_START, pkg_local, NULL);
+			_pacman_log(PM_LOG_FLOW1, _("removing package %s-%s"), pkg_local->name, pkg_local->version);
+
+			/* run the pre-remove scriptlet if it exists */
+			if(pkg_local->scriptlet && !(trans->flags & PM_TRANS_FLAG_NOSCRIPTLET)) {
+				snprintf(pm_install, PATH_MAX, "%s/%s-%s/install", db_local->path, pkg_local->name, pkg_local->version);
+				_pacman_runscriptlet(handle->root, pm_install, "pre_remove", pkg_local->version, NULL, trans);
+			}
+		}
+
+		if(!(trans->flags & PM_TRANS_FLAG_DBONLY)) {
+			int filenum = _pacman_list_count(pkg_local->files);
+			_pacman_log(PM_LOG_FLOW1, _("removing files"));
+
+			/* iterate through the list backwards, unlinking files */
+			for(lp = _pacman_list_last(pkg_local->files); lp; lp = lp->prev) {
+				int nb = 0;
+                double percent = 0;
+				char *file = lp->data;
+				char *hash_orig = _pacman_pkg_fileneedbackup(pkg_local, file);
+
+				if (position != 0) {
+				percent = (double)position / filenum;
+				}
+				if(!_pacman_strempty(hash_orig)) {
+					nb = 1;
+				}
+				FREE(hash_orig);
+				if(!nb && trans->type == PM_TRANS_TYPE_UPGRADE) {
+					/* check noupgrade */
+					if(_pacman_list_is_strin(file, handle->noupgrade)) {
+						nb = 1;
+					}
+				}
+				snprintf(line, PATH_MAX, "%s%s", handle->root, file);
+				if(lstat(line, &buf)) {
+					_pacman_log(PM_LOG_DEBUG, _("file %s does not exist"), file);
+					continue;
+				}
+				if(S_ISDIR(buf.st_mode)) {
+					if(rmdir(line)) {
+						/* this is okay, other packages are probably using it. */
+						_pacman_log(PM_LOG_DEBUG, _("keeping directory %s"), file);
+					} else {
+						_pacman_log(PM_LOG_FLOW2, _("removing directory %s"), file);
+					}
+				} else {
+					/* check the "skip list" before removing the file.
+					 * see the big comment block in db_find_conflicts() for an
+					 * explanation. */
+					int skipit = 0;
+					pmlist_t *j;
+					for(j = trans->skiplist; j; j = j->next) {
+						if(!strcmp(file, (char*)j->data)) {
+							skipit = 1;
+						}
+					}
+					if(skipit) {
+						_pacman_log(PM_LOG_FLOW2, _("skipping removal of %s as it has moved to another package"),
+							file);
+					} else {
+						/* if the file is flagged, back it up to .pacsave */
+						if(nb) {
+							if(trans->type == PM_TRANS_TYPE_UPGRADE) {
+								/* we're upgrading so just leave the file as is. pacman_add() will handle it */
+							} else {
+								if(!(trans->flags & PM_TRANS_FLAG_NOSAVE)) {
+									char newpath[PATH_MAX];
+									snprintf(newpath, PATH_MAX, "%s.pacsave", line);
+									rename(line, newpath);
+									_pacman_log(PM_LOG_WARNING, _("%s saved as %s"), line, newpath);
+								} else {
+									_pacman_log(PM_LOG_FLOW2, _("unlinking %s"), file);
+									if(unlink(line)) {
+										_pacman_log(PM_LOG_ERROR, _("cannot remove file %s"), file);
+									}
+								}
+							}
+						} else {
+							_pacman_log(PM_LOG_FLOW2, _("unlinking %s"), file);
+							/* Need at here because we count only real unlinked files ? */
+							PROGRESS(trans, PM_TRANS_PROGRESS_REMOVE_START, pkg_local->name, (int)(percent * 100), howmany, howmany - remain + 1);
+							position++;
+							if(unlink(line)) {
+								_pacman_log(PM_LOG_ERROR, _("cannot remove file %s"), file);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		PROGRESS(trans, PM_TRANS_PROGRESS_REMOVE_START, pkg_local->name, 100, howmany, howmany - remain + 1);
+		if(trans->type != PM_TRANS_TYPE_UPGRADE) {
+			/* run the post-remove script if it exists */
+			if(pkg_local->scriptlet && !(trans->flags & PM_TRANS_FLAG_NOSCRIPTLET)) {
+				/* must run ldconfig here because some scriptlets fail due to missing libs otherwise */
+				_pacman_ldconfig(handle->root);
+				snprintf(pm_install, PATH_MAX, "%s/%s-%s/install", db_local->path, pkg_local->name, pkg_local->version);
+				_pacman_runscriptlet(handle->root, pm_install, "post_remove", pkg_local->version, NULL, trans);
+			}
+		}
+
+		/* remove the package from the database */
+		_pacman_log(PM_LOG_FLOW1, _("updating database"));
+		_pacman_log(PM_LOG_FLOW2, _("removing database entry '%s'"), pkg_local->name);
+		if(_pacman_db_remove(db_local, pkg_local) == -1) {
+			_pacman_log(PM_LOG_ERROR, _("could not remove database entry %s-%s"), pkg_local->name, pkg_local->version);
+		}
+		if(_pacman_db_remove_pkgfromcache(db_local, pkg_local) == -1) {
+			_pacman_log(PM_LOG_ERROR, _("could not remove entry '%s' from cache"), pkg_local->name);
+		}
+
+		/* update dependency packages' REQUIREDBY fields */
+		_pacman_log(PM_LOG_FLOW2, _("updating dependency packages 'requiredby' fields"));
+		for(lp = pkg_local->depends; lp; lp = lp->next) {
+			pmpkg_t *depinfo = NULL;
+			pmdepend_t depend;
+			char *data;
+			if(_pacman_splitdep((char*)lp->data, &depend)) {
+				continue;
+			}
+			/* if this dependency is in the transaction targets, no need to update
+			 * its requiredby info: it is in the process of being removed (if not
+			 * already done!)
+			 */
+			if(_pacman_pkg_isin(depend.name, trans->packages)) {
+				continue;
+			}
+			depinfo = _pacman_db_get_pkgfromcache(db_local, depend.name);
+			if(depinfo == NULL) {
+				/* look for a provides package */
+				pmlist_t *provides = _pacman_db_whatprovides(db_local, depend.name);
+				if(provides) {
+					/* TODO: should check _all_ packages listed in provides, not just
+					 *			 the first one.
+					 */
+					/* use the first one */
+					depinfo = _pacman_db_get_pkgfromcache(db_local, ((pmpkg_t *)provides->data)->name);
+					FREELISTPTR(provides);
+				}
+				if(depinfo == NULL) {
+					_pacman_log(PM_LOG_ERROR, _("could not find dependency '%s'"), depend.name);
+					/* wtf */
+					continue;
+				}
+			}
+			/* splice out this entry from requiredby */
+			depinfo->requiredby = _pacman_list_remove(_pacman_pkg_getinfo(depinfo, PM_PKG_REQUIREDBY), pkg_local->name, str_cmp, (void **)&data);
+			FREE(data);
+			_pacman_log(PM_LOG_DEBUG, _("updating 'requiredby' field for package '%s'"), depinfo->name);
+			if(_pacman_db_write(db_local, depinfo, INFRQ_DEPENDS)) {
+				_pacman_log(PM_LOG_ERROR, _("could not update 'requiredby' database entry %s-%s"),
+					depinfo->name, depinfo->version);
+			}
+		}
+
+		if(trans->type != PM_TRANS_TYPE_UPGRADE) {
+			EVENT(trans, PM_TRANS_EVT_REMOVE_DONE, pkg_local, NULL);
+		}
+	}
+
+	/* run ldconfig if it exists */
+	if((trans->type != PM_TRANS_TYPE_UPGRADE) && (handle->trans->state != STATE_INTERRUPTED)) {
+		_pacman_ldconfig(handle->root);
+	}
+
+	return(0);
+}
+
 int _pacman_trans_commit(pmtrans_t *trans, pmlist_t **data)
 {
 	/* Sanity checks */
@@ -1105,6 +1372,11 @@ int _pacman_trans_commit(pmtrans_t *trans, pmlist_t **data)
 	} else {
 	if(trans->type & PM_TRANS_TYPE_ADD) {
 		if(_pacman_add_commit(trans, data) == -1) {
+			return -1;
+		}
+	}
+	if(trans->type == PM_TRANS_TYPE_REMOVE) {
+		if(_pacman_remove_commit(trans, data) == -1) {
 			return -1;
 		}
 	}
